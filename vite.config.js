dfd5002ffc1b1e0a3f7fb5325c165631b130da96
David Camick <david@MacBookPro.attlocal.net>
2025-03-11 16:35:10 -0400

    sfx page 5

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: '',  // This ensures assets are loaded relative to index.html
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          // Keep the original file names for assets in the public directory
+          if (assetInfo.name.includes('/assets/')) {
+            return assetInfo.name
+          }
+          return `assets/[name]-[hash][extname]`
+        }
+      }
+    }
+  }
 })
