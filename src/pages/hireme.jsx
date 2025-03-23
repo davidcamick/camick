fc21a997b720a9612550472b06dc834b20d51025
David Camick <david@MacBookPro.attlocal.net>
2025-03-23 16:04:42 -0400

    new cover vid

--- src/pages/hireme.jsx
+++ src/pages/hireme.jsx
@@ -135,7 +135,7 @@ export default function HireMe() {
             {/* Replaced placeholder with Vimeo embed */}
             <div style={{padding:'56.25% 0 0 0', position:'relative'}}>
               <iframe 
-                src="https://player.vimeo.com/video/1061880871?h=21ba0ad8ea&badge=0&autopause=0&player_id=0&app_id=58479" 
+                src="https://player.vimeo.com/video/1068645245?h=3cbbac1d8f&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" 
                 frameBorder="0" 
                 allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media" 
                 style={{position:'absolute', top:0, left:0, width:'100%', height:'100%', pointerEvents: 'auto'}} 
